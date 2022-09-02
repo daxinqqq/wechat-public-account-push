e0fc51ab466500b5f27a5d7bd5a4c99c792a5c2d
daxinqqq <32762677+daxinqqq@users.noreply.github.com>
2022-09-02 15:24:55 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -108,19 +108,19 @@ export const config = {
 
     }
 
-    {{date.DATA}}  
-    城市：{{city.DATA}}  
-    天气：{{weather.DATA}}  
-    最低气温: {{min_temperature.DATA}}  
-    最高气温: {{max_temperature.DATA}}  
-    今天是我们恋爱的第{{love_day.DATA}}天
-    今天是我们结婚的第{{marry_day.DATA}}天
-    {{birthday_message.DATA}}
-
-    {{note_en.DATA}}  
-    {{note_ch.DATA}}
-
-    共推送 {{need_post_num.DATA}}  人
-    成功: {{success_post_num.DATA}} | 失败: {{fail_post_num.DATA}}
-    成功用户: {{success_post_ids.DATA}}
-    失败用户: {{fail_post_ids.DATA}}
+    //{{date.DATA}}  
+    //城市：{{city.DATA}}  
+    //天气：{{weather.DATA}}  
+    //最低气温: {{min_temperature.DATA}}  
+    //最高气温: {{max_temperature.DATA}}  
+    //今天是我们恋爱的第{{love_day.DATA}}天
+    //今天是我们结婚的第{{marry_day.DATA}}天
+    //{{birthday_message.DATA}}
+//
+    //{{note_en.DATA}}  
+    //{{note_ch.DATA}}
+//
+    //共推送 {{need_post_num.DATA}}  人
+    //成功: {{success_post_num.DATA}} | 失败: {{fail_post_num.DATA}}
+    //成功用户: {{success_post_ids.DATA}}
+    //失败用户: {{fail_post_ids.DATA}}
