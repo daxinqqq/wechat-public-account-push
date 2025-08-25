e3a90669cf2f2acccda5c3cf9f30e0b9289ed318
daxinqqq <32762677+daxinqqq@users.noreply.github.com>
2025-08-25 14:17:53 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -54,7 +54,7 @@ export const config = {
     FESTIVALS: [
       {"type": "生日", "name": "老婆", "year": "1995", "date": "12-25"},
       {"type": "节日", "name": "恋爱纪念日", "year": "2014", "date": "09-10"},
-      {"type": "生日", "name": "老妈", "year": "1970", "date": "12-10"},
+      {"type": "节日", "name": "结婚纪念日", "year": "2025", "date": "07-20"},
     ],
 
     /**
